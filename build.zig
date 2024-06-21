aa686a123c409654c65de6157b46985b8dc55e41
alexander zillion <alex@alexzillion.com>
2024-06-21 14:20:13 -0500

    hinc itur ad astra

--- build.zig
+++ build.zig
@@ -1,43 +1,17 @@
 const std = @import("std");
-const rlz = @import("raylib-zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib_dep = b.dependency("raylib-zig", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    const exe = b.addExecutable(.{ .name = "gravitas", .root_source_file = b.path("src/main.zig"), .optimize = optimize, .target = target });
 
-    const raylib = raylib_dep.module("raylib");
-    const raymath = raylib_dep.module("raymath");
-    const raylib_artifact = raylib_dep.artifact("raylib");
+    @import("system_sdk").addLibraryPathsTo(exe);
+    @import("zgpu").addLibraryPathsTo(exe);
 
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const exe_lib = rlz.emcc.compileForEmscripten(b, "gravitas", "src/gravitas.zig", target, optimize);
-
-        exe_lib.linkLibrary(raylib_artifact);
-        exe_lib.root_module.addImport("raylib", raylib);
-        exe_lib.root_module.addImport("raymath", raymath);
-
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
-
-        b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
-        run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run gravitas");
-        run_option.dependOn(&run_step.step);
-        return;
-    }
-
-    const exe = b.addExecutable(.{ .name = "gravitas", .root_source_file = .{ .path = "src/gravitas.zig" }, .optimize = optimize, .target = target });
-
-    exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-    exe.root_module.addImport("raymath", raymath);
+    const zgpu = b.dependency("zgpu", .{});
+    exe.root_module.addImport("zgpu", zgpu.module("root"));
+    exe.linkLibrary(zgpu.artifact("zdawn"));
 
     const run_cmd = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run gravitas");
